b92b7993c50dd493228c81d71d2407cb426f3b50
briannkhata <briannkhata@gmail.com>
2023-03-26 11:26:20 +0200

    initial commit

---
+++ sever.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app  = express();
+
+const mongoose = require("mongoose")
+
+
+mongoose.connect("mongodb://localhost:27017//nodevue",(req,res,err) => {
+
+if(err){
+    console.log(err);
+}else{
+    console.log("connected to the database")
+}
+
+})
+
+
+
+
+
+
+app.listen(5000,() => {
+    console.log('server started')
+})
