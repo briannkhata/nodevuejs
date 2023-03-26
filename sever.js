6ff0bb71da94bf7ed380c65aea82209694dc4e2f
briannkhata <briannkhata@gmail.com>
2023-03-26 11:30:56 +0200

    house keeping

--- sever.js
+++ sever.js
@@ -1,24 +1,20 @@
-const express = require("express")
-const app  = express();
+const express = require("express");
+const app = express();
 
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
+mongoose.connect(
+  "mongodb://localhost:27017//nodevue",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (req, res, err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("connected to the database");
+    }
+  }
+);
 
-mongoose.connect("mongodb://localhost:27017//nodevue",(req,res,err) => {
-
-if(err){
-    console.log(err);
-}else{
-    console.log("connected to the database")
-}
-
-})
-
-
-
-
-
-
-app.listen(5000,() => {
-    console.log('server started')
-})
+app.listen(5000, () => {
+  console.log("server started");
+});
