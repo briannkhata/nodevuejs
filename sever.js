d355a54cdf6b374ba049a913056e2d46b4f4e514
briannkhata <briannkhata@gmail.com>
2023-03-26 11:53:37 +0200

    added routes

--- sever.js
+++ sever.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes/routes");
 
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb://localhost:27017//nodevue",
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (req, res, err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("connected to the database");
-    }
-  }
-);
+mongoose
+  .connect("mongodb://localhost:27017/nodevue", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to the database"))
+  .catch((err) => console.error("Connection error:", err));
 
+app.use(express.json());
 app.listen(5000, () => {
   console.log("server started");
 });
