1fc8738d6099f51045b82c8d0f8d42239d8d7e62
Brian Nkhata <bnkhata@mra.mw>
2023-03-27 12:16:45 +0200

    added vue routes

--- client/frontend/src/components/EmployeeCrud.vue
+++ client/frontend/src/components/EmployeeCrud.vue
@@ -1,22 +1,22 @@
 <template>
-    <div id="app">
+    <div id="EmployeeCrud">
         <div>
             <h1>Employee Registration</h1>
             <div>
                 <form @submit.prevent="save">
                     <div class="form-group">
                         <label for="name">Name:</label>
-                        <input type="text" v-model="name" class="form-control">
+                        <input type="text" v-model="employee.name" class="form-control">
                     </div>
 
                     <div class="form-group">
                         <label for="phone">Phone:</label>
-                        <input type="tel" v-model="phone" class="form-control">
+                        <input type="tel" v-model="employee.phone" class="form-control">
                     </div>
 
                     <div class="form-group">
                         <label for="address">Address:</label>
-                        <input type="text" v-model="address" class="form-control">
+                        <input type="text" v-model="employee.address" class="form-control">
                     </div>
                     <button type="submit" class="btn btn-primary">Save</button>
                 </form>
@@ -37,14 +37,14 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Data 1</td>
-                        <td>Data 2</td>
-                        <td>Data 3</td>
-                        <td>Data 3</td>
-                        <td>html
-                            <button type="button" class="btn btn-primary">Edit</button>
-                            <button type="button" class="btn btn-danger">Delete</button>
+                    <tr v-for="employee in result" v-bind:key="employee.id">
+                        <td>{{ employee._id }}</td>
+                        <td>{{ employee.name }}</td>
+                        <td>{{ employee.address }}</td>
+                        <td>{{ employee.phone }}</td>
+                        <td>
+                            <button type="button" class="btn btn-primary" @click="edit(employee.id)">Edit</button>
+                            <button type="button" class="btn btn-danger" @click="remove(employee.id)">Delete</button>
                         </td>
                     </tr>
                 </tbody>
@@ -55,9 +55,72 @@
 </template>
 
 <script>
+
+import Vue from 'vue'
+import axios from 'axios'
+
+Vue.use(axios)
+
 export default {
-    name: "App",
-};
+  name: 'EmployeeCrud',
+  data () {
+    return {
+      result: {},
+      employee: {
+        _id: '',
+        name: '',
+        address: '',
+        phone: ''
+      }
+    }
+  },
+  created () {
+    this.EmployeeLoad()
+  },
+  mounted () {
+    console.log('mounted() called ....')
+  },
+  methods: {
+    EmployeeLoad () {
+      var page = 'http://localhost:5000/user/getAll'
+      axios.get(page).then(
+        ({data}) => {
+          console.log(data)
+          this.result = data.data
+        }
+      )
+    },
+
+    saveData () {
+      axios.post('http://localhost/user/create/', this.employee)
+        .then(
+          ({data}) => {
+            alert('User seved')
+            this.EmployeeLoad()
+          }
+        )
+    },
+
+    edit (employee) {
+      this.employee = employee
+    },
+
+    updateData () {
+      var editrecords = 'http://localhost:5000/user/update/' + this.employee._id
+      axios.patch(editrecords.this.employee).then(
+        ({data}) => {
+          this.employee.name = ''
+          this.employee.address = ''
+          this.employee.phone = ''
+          this.id = ''
+          alert('Updated')
+          this.EmployeeLoad()
+        }
+      )
+    }
+
+  }
+}
 </script>
 
 <style>
