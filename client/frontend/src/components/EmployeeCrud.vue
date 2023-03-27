c87b450834c2565fab71f7c5fa50fc935a0e6590
Brian Nkhata <bnkhata@mra.mw>
2023-03-27 11:42:43 +0200

    added vue component

---
+++ client/frontend/src/components/EmployeeCrud.vue
@@ -0,0 +1,72 @@
+<template>
+    <div id="app">
+        <div>
+            <h1>Employee Registration</h1>
+            <div>
+                <form @submit.prevent="save">
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" v-model="name" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="phone">Phone:</label>
+                        <input type="tel" v-model="phone" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="address">Address:</label>
+                        <input type="text" v-model="address" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </form>
+            </div>
+        </div>
+
+        <div class="container mt-5">
+            <h1>Employeee List</h1>
+            <p>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Phone</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Data 1</td>
+                        <td>Data 2</td>
+                        <td>Data 3</td>
+                        <td>Data 3</td>
+                        <td>html
+                            <button type="button" class="btn btn-primary">Edit</button>
+                            <button type="button" class="btn btn-danger">Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+};
+</script>
+
+<style>
+#app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
